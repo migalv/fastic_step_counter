93b052094bcb03d26b91f99135649faa47e73617
migalv <migalv007@gmail.com>
2021-06-10 13:57:15 +0200

    Specify the Health Source Contract & its failures

---
+++ lib/domain/health_source/health_source_failure.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'health_source_failure.freezed.dart';
+
+@freezed
+
+/// Union class to represent any failure that can happen in the Health Source
+abstract class HealthSourceFailure with _$HealthSourceFailure {
+  /// When an unexpected error ocurs withing the Health Source.
+  ///
+  /// Catching this type of errors helps determining the source of the error
+  /// much more effectively.
+  const factory HealthSourceFailure.unexpectedError() = _UnexpectedError;
+}
